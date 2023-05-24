c6b4123f0b310b55c8149083e2181746b1e947ae
rchozas <rchglez@gmail.com>
2023-05-24 17:14:48 +0200

    version2

--- P4/S10/main.js
+++ P4/S10/main.js
@@ -2,3 +2,9 @@
 const electron = require('electron');
 
 console.log("Arrancando electron...");
+
+//-- Punto de entrada. En cuanto electron está listo,
+//-- ejecuta esta función
+electron.app.on('ready', ()=>{
+    console.log("Evento Ready!")
+});
