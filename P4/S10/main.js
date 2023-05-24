48ece12aa592265e6f8e232433c34f71e30044a3
rchozas <rchglez@gmail.com>
2023-05-24 17:20:08 +0200

    main version 3

--- P4/S10/main.js
+++ P4/S10/main.js
@@ -3,8 +3,19 @@ const electron = require('electron');
 
 console.log("Arrancando electron...");
 
+//-- Variable para acceder a la ventana principal
+//-- Se pone aquí para que sea global al módulo principal
+let win = null;
+
 //-- Punto de entrada. En cuanto electron está listo,
 //-- ejecuta esta función
-electron.app.on('ready', ()=>{
-    console.log("Evento Ready!")
+electron.app.on('ready', () => {
+    console.log("Evento Ready!");
+
+    //-- Crear la ventana principal de nuestra aplicación
+    win = new electron.BrowserWindow({
+        width: 600,  //-- Anchura 
+        height: 400  //-- Altura
+    });
+
 });
