37e5666e81d7ee1837e8c5de4625ea5ff91b8aa8
rchozas <rchglez@gmail.com>
2023-05-25 13:23:42 +0200

    saca info pedida

--- P4/index.js
+++ P4/index.js
@@ -13,9 +13,9 @@ const print = document.getElementById("print");
 //-- Acceder a la API de node para obtener la info
 //-- Sólo es posible si nos han dado permisos desde
 //-- el proceso princpal
-info1.textContent = process.arch;
-info2.textContent = process.platform;
-info3.textContent = process.cwd();
+info1.textContent = process.versions.node;
+info2.textContent = process.versions.chrome;
+info3.textContent = process.versions.electron;
 
 
 btn_test.onclick = () => {
